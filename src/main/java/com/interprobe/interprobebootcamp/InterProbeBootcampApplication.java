ab2acdbe605f445416ec194e12577bb8a8c5365d
theLostCode <lostcoding11@ghmail.com>
2022-07-01 21:46:12 +0300

    start

---
+++ src/main/java/com/interprobe/interprobebootcamp/InterProbeBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.interprobe.interprobebootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterProbeBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterProbeBootcampApplication.class, args);
+    }
+
+}
