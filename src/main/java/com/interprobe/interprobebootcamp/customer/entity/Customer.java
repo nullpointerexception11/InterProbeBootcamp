2f4651e048f0d53621eaa2ada5d92b7acb7ab2b4
theLostCode <lostcoding11@ghmail.com>
2022-07-04 09:52:22 +0300

    customer entity created

---
+++ src/main/java/com/interprobe/interprobebootcamp/customer/entity/Customer.java
@@ -0,0 +1,55 @@
+package com.interprobe.interprobebootcamp.customer.entity;
+
+import com.interprobe.interprobebootcamp.customer.enums.EnumStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Date;
+
+/**
+ * @author theLostCode
+ * @Date 7/4/2022
+ */
+@Entity
+@Table(name = "CUSTOMER")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(generator = "Customer")
+    @SequenceGenerator(name = "Customer", sequenceName = "CUSTOMER_ID_SEQ")
+    private Long id;
+
+    @Column(name = "NAME", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "SURNAME", length = 100, nullable = false)
+    private String surname;
+
+    @Column(name = "USERNAME", length = 15, nullable = false, updatable = false)
+    private String username;
+
+    @Column(name = "PASSWORD", length = 400, nullable = false)
+    private String password;
+
+    @Column(name = "PHONE_NUMBER", length = 15)
+    private String phoneNumber;
+
+    @Email
+    @Column(name = "EMAIL", length = 50)
+    private String email;
+
+    @Column(name = "BIRTH_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Column(name = "STATUS", length = 30)
+    @Enumerated(EnumType.STRING)
+    private EnumStatus status;
+
+    @Column(name = "CANCEL_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date cancelDate;
+}
