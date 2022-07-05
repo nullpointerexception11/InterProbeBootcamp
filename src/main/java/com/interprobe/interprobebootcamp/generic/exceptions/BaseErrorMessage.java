e0aca822627a839fc433676b133accec618c4ce6
theLostCode <lostcoding11@gmail.com>
2022-07-05 23:25:19 +0300

    base error message created

---
+++ src/main/java/com/interprobe/interprobebootcamp/generic/exceptions/BaseErrorMessage.java
@@ -0,0 +1,10 @@
+package com.interprobe.interprobebootcamp.generic.exceptions;
+
+/**
+ * @author theLostCode
+ * @Date 7/5/2022
+ */
+public interface BaseErrorMessage {
+
+    String getMessage();
+}
