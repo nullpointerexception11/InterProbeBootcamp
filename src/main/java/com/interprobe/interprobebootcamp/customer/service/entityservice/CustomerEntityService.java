607393088dabaea4dfeca1f76f6bc42005f31eae
theLostCode <lostcoding11@gmail.com>
2022-07-07 22:03:12 +0300

    customer entity service updated

--- src/main/java/com/interprobe/interprobebootcamp/customer/service/entityservice/CustomerEntityService.java
+++ src/main/java/com/interprobe/interprobebootcamp/customer/service/entityservice/CustomerEntityService.java
@@ -1,8 +1,17 @@
 package com.interprobe.interprobebootcamp.customer.service.entityservice;
 
+import com.interprobe.interprobebootcamp.customer.dao.CustomerDao;
+import com.interprobe.interprobebootcamp.customer.entity.Customer;
+import com.interprobe.interprobebootcamp.generic.service.BaseEntityService;
+import org.springframework.stereotype.Service;
+
 /**
  * @author theLostCode
  * @Date 7/5/2022
  */
-public class CustomerEntityService {
+@Service
+public class CustomerEntityService extends BaseEntityService<Customer, CustomerDao> {
+    public CustomerEntityService(CustomerDao dao) {
+        super(dao);
+    }
 }
